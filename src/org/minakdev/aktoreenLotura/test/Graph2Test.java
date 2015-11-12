667d446433c5c6662a1d9e416c106a4fbf6a6224
DOCENCIA <DOCENCIA@U026339.xa.bp.ehu.es>
2015-11-12 09:57:44 +0100

    J-Unita igota

---
+++ src/org/minakdev/aktoreenLotura/test/Graph2Test.java
@@ -0,0 +1,34 @@
+package org.minakdev.aktoreenLotura.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Graph2Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGrafoaSortu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKonektatuta() {
+		fail("Not yet implemented");
+	}
+
+}
